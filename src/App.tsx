b58eae1c53230ec7c2b4d88452c2835ede8830d7
Erpix3lt <maximilian.schmalenbach@hotmail.com>
2025-10-24 17:16:14 +0200

    (app): mobile optimisation

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ export default function App() {
         </div>
 
         {/* Preview - background layer with lower z-index */}
-        <div id="preview" className="z-0 ml-96"></div>
+        <div id="preview" className="z-0 ml-0 md:ml-96"></div>
 
         {/* Navigation - fixed overlay on the left */}
-        <div className="fixed top-0 left-0 z-10 pt-30 m-10">
+        <div className="fixed top-0 left-0 z-10 pt-30 m-10 md:block hidden">
           <Navigation
             elements={navigationElements}
             isPreviewReady={isPreviewReady}
